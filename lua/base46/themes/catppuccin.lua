1a7cc78599fa2b3e2a84d10ca4f36264e56d22a9
Julius Kluge <Julius.Kluge@tq-group.com>
2023-05-23 17:30:22 +0200

    Revert "change catppuccin colors"
    This reverts commit 6393fe053f1021d8faa84331b6bb14af1caff9c6.

--- lua/base46/themes/catppuccin.lua
+++ lua/base46/themes/catppuccin.lua
@@ -54,8 +54,8 @@ M.base_16 = {
 }
 
 M.polish_hl = {
-  ["@variable"] = { fg = M.base_30.white },
-  ["@property"] = { fg = M.base_30.red },
+  ["@variable"] = { fg = M.base_30.lavender },
+  ["@property"] = { fg = M.base_30.teal },
   ["@variable.builtin"] = { fg = M.base_30.red },
 }
 
